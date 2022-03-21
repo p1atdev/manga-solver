790c683156605b5f2c365c3b849f644a25b8e48e
p1atdev <shu_teiei@outlook.jp>
2022-03-22 01:05:47 +0900

    chore: update test

--- tests/local.test.ts
+++ tests/local.test.ts
@@ -1,6 +1,12 @@
 import { getImage, Solver } from "../util/solver.ts";
 
 Deno.test("Decode image from local (./assets/0.jpg)", async () => {
+  // check file exists
+  const file = await Deno.stat("./assets/0.jpg");
+  if (!file.isFile) {
+    return;
+  }
+
   const image = await getImage(
     "./assets/0.jpg",
   );
